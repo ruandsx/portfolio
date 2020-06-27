6847727d9aa19b3f78c2914ff7617207eef7266d
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-27 11:48:21 -0300

    Updates

---
+++ precache-manifest.ae6037db5f55ab3939fa85d3c0e6a2a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28118259d7646117b466b6e2843d4442",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db59edca44c74f60f4c",
+    "url": "/static/js/2.0cb4aba8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0cb4aba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43e8751a4199f234291f",
+    "url": "/static/js/main.dfbdf791.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
