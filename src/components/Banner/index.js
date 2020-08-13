8a0e1800a21808180a33c563ca9a084f08acad59
Ruan Xavier <ruandsxavier@gmail.com>
2020-08-13 15:28:08 -0300

    changing banner texts

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -12,8 +12,8 @@ const Banner = () => {
           src="https://avatars1.githubusercontent.com/u/38995610?s=460&u=d4d5c903f2f3c0e397c5a9fa3b54acf23a534ca9&v=4"
         />
       </Avatar>
-      <h1> Ruan Xavier - Full Stack Developer </h1>
-      <h2> Web and Mobile </h2>
+      <h1> Ruan Xavier </h1>
+      <h2> Web and Mobile development </h2>
     </Container>
   );
 };
